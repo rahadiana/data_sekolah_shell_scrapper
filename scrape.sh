5b9523e7652475295f127286f039af49be0bcb71
Administrator <>
2021-10-07 22:39:58 +0700

    Update scrape.sh

--- scrape.sh
+++ scrape.sh
@@ -3,7 +3,7 @@ startup()
 {
 echo "startup start"
 mkdir -p data_sekolah
-wget  "https://dapo.kemdikbud.go.id/sp" -O cache/tmp/tiktok.txt 
+wget "https://dapo.kemdikbud.go.id/sp" -O cache/tmp/tiktok.txt 
 xmllint --noout --html -xpath '//*[@id="selectSemester"]/option[1]/@value'  2>/dev/null cache/tmp/tiktok.txt | sed -e "s/ //; s/value=\"//; s/\"//;" > cache/tmp/smstrnow.txt
 bbb=$(cat cache/tmp/smstrnow.txt )
 mkdir -p data_sekolah/all/province/$bbb
@@ -12,14 +12,14 @@ mkdir -p data_sekolah/all/region/$bbb
 echo "startup end"
 clear
 }
-bbb=$(cat cache/tmp/smstrnow.txt )
 aabbcc() {
+bbb=$(cat cache/tmp/smstrnow.txt )
 echo "abc start"
-wget -q "https://dapo.kemdikbud.go.id/rekap/dataSekolah?id_level_wilayah=0&kode_wilayah=000000&semester_id=$bbb" -O "data_sekolah/all/province/$bbb/$bbb.json"
+wget "https://dapo.kemdikbud.go.id/rekap/dataSekolah?id_level_wilayah=0&kode_wilayah=000000&semester_id=$bbb" -O "data_sekolah/all/province/$bbb/$bbb.json"
 cat data_sekolah/all/province/$bbb/$bbb.json | jq -r '.[].kode_wilayah' > cache/tmp/kodewilayah.txt
 cat  cache/tmp/kodewilayah.txt | sed "s/^/mkdir -p data_sekolah\/province\/$bbb\//" | bash
 mkdir -p data_sekolah/city/$bbb
-cat  cache/tmp/kodewilayah.txt|sed "s/ //g" | sed "s/^/wget -nc  'https:\/\/dapo.kemdikbud.go.id\/rekap\/dataSekolah?id_level_wilayah=1\&kode_wilayah=/; s/$/' -O data_sekolah\/province\/$bbb\//" > cache/tmp/city.txt
+cat cache/tmp/kodewilayah.txt|sed "s/ //g" | sed "s/^/wget -nc 'https:\/\/dapo.kemdikbud.go.id\/rekap\/dataSekolah?id_level_wilayah=1\&kode_wilayah=/; s/$/' -O data_sekolah\/province\/$bbb\//" > cache/tmp/city.txt
 paste -d "" cache/tmp/city.txt  cache/tmp/kodewilayah.txt cache/tmp/kodewilayah.txt | sed 's/$/.json/' | sed 's/  .json/.json/g; s/  /\//g' | bash
 mkdir -p cache/tmp/data_sekolah
 rm cache/tmp/data_sekolah/*.json
@@ -44,7 +44,7 @@ echo "findDetail start"
 find data_sekolah/city/$bbb -name '*.json' -exec cat {} \; | jq '.' > data_sekolah/all/city/$bbb/$bbb.json
 cat data_sekolah/all/city/$bbb/$bbb.json | jq -r '.[].kode_wilayah' | sed 's/ //g' > cache/tmp/allkota.txt
 cat cache/tmp/allkota.txt | sed "s/^/mkdir -p data_sekolah\/region\/$bbb\//g" | bash
-cat cache/tmp/allkota.txt | sed "s/^/timeout 10 wget -nc  'https:\/\/dapo.kemdikbud.go.id\/rekap\/progresSP?id_level_wilayah=3\&kode_wilayah=/; s/$/\&smester_id=$bbb\&bentuk_pendidikan_id=' -O data_sekolah\/region\/$bbb/" > cache/tmp/data_sekolah/region.txt
+cat cache/tmp/allkota.txt | sed "s/^/timeout 10 wget -nc 'https:\/\/dapo.kemdikbud.go.id\/rekap\/progresSP?id_level_wilayah=3\&kode_wilayah=/; s/$/\&smester_id=$bbb\&bentuk_pendidikan_id=' -O data_sekolah\/region\/$bbb/" > cache/tmp/data_sekolah/region.txt
 paste -d "|" cache/tmp/data_sekolah/region.txt cache/tmp/allkota.txt cache/tmp/allkota.txt  | sed 's/$/.json \&/'  | sed 's/|/\//g;'  | sed -e '0~10 s/$/\nwait/g;' | bash
 echo "findDetail end"
 clear
@@ -53,7 +53,7 @@ clear
 #| jq '[try .[] | {id_origin_news: .nama_kabkot , sumber: "tirto.com"}]'
 startup
 aabbcc
-FindCity
-FindDetail
-echo "finish"
+#FindCity
+#FindDetail
+#echo "finish"
 exit 1
