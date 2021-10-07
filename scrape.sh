919ec33a2cea0280e9fafcf8cccb16f11d410080
Administrator <>
2021-10-07 22:09:26 +0700

    f

--- scrape.sh
+++ scrape.sh
@@ -1,11 +1,25 @@
 
 startup()
 {
+
+
+echo "startup start"
+mkdir -p data_sekolah
+wget  -q "https://dapo.kemdikbud.go.id/sp" -O /tmp/tiktok.txt 
+xmllint --noout --html -xpath '//*[@id="selectSemester"]/option[1]/@value'  2>/dev/null /tmp/tiktok.txt | sed -e "s/ //; s/value=\"//; s/\"//;" > /tmp/smstrnow.txt
+bbb=$(cat /tmp/smstrnow.txt )
+mkdir -p data_sekolah/all/province/$bbb
+mkdir -p data_sekolah/all/city/$bbb
+mkdir -p data_sekolah/all/region/$bbb
+echo "startup end"
+clear
+
+bbb=$(cat /tmp/smstrnow.txt )
 echo "startup start"
 sudo mkdir -p data_sekolah
 sudo mkdir -p cache/tmp/
-wget  -q "https://dapo.kemdikbud.go.id/sp" -O cache/tmp/ tiktok.txt 
-xmllint --noout --html -xpath '//*[@id="selectSemester"]/option[1]/@value'  2>/dev/null cache/tmp/ tiktok.txt | sed -e "s/ //; s/value=\"//; s/\"//;" > cache/tmp/ smstrnow.txt
+wget  -q "https://dapo.kemdikbud.go.id/sp" -O cache/tmp/tiktok.txt 
+xmllint --noout --html -xpath '//*[@id="selectSemester"]/option[1]/@value'  2>/dev/null cache/tmp/tiktok.txt | sed -e "s/ //; s/value=\"//; s/\"//;" > cache/tmp/smstrnow.txt
 bbb=$(cat cache/tmp/ smstrnow.txt )
 sudo mkdir -p data_sekolah/all/province/$bbb
 sudo mkdir -p data_sekolah/all/city/$bbb
