8dc52d7b2917c57a9c0dd74b4f819c8ab2f9855a
Administrator <>
2021-10-07 22:18:18 +0700

    Update scrape.sh

--- scrape.sh
+++ scrape.sh
@@ -1,5 +1,5 @@
 
-
+sudo mkdir -p cache/tmp/
 echo "startup start"
 mkdir -p data_sekolah
 wget  -q "https://dapo.kemdikbud.go.id/sp" -O cache/tmp/tiktok.txt 
@@ -12,7 +12,7 @@ echo "startup end"
 
 echo "startup start"
 sudo mkdir -p data_sekolah
-sudo mkdir -p cache/tmp/
+
 wget  -q "https://dapo.kemdikbud.go.id/sp" -O cache/tmp/tiktok.txt 
 xmllint --noout --html -xpath '//*[@id="selectSemester"]/option[1]/@value'  2>/dev/null cache/tmp/tiktok.txt | sed -e "s/ //; s/value=\"//; s/\"//;" > cache/tmp/smstrnow.txt
 bbb=$(cat cache/tmp/ smstrnow.txt )
