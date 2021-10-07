f887ee4d0085b66202913cb47118975337043134
Administrator <>
2021-10-07 22:25:06 +0700

    waduh

--- scrape.sh
+++ scrape.sh
@@ -5,17 +5,15 @@ mkdir -p data_sekolah
 wget "https://dapo.kemdikbud.go.id/sp" -O cache/tmp/tiktok.txt 
 xmllint --noout --html -xpath '//*[@id="selectSemester"]/option[1]/@value'  2>/dev/null cache/tmp/tiktok.txt | sed -e "s/ //; s/value=\"//; s/\"//;" > cache/tmp/smstrnow.txt
 bbb=$(cat cache/tmp/smstrnow.txt )
-mkdir -p data_sekolah/all/province/$bbb
-mkdir -p data_sekolah/all/city/$bbb
-mkdir -p data_sekolah/all/region/$bbb
+sudo mkdir -p data_sekolah/all/province/$bbb
+sudo mkdir -p data_sekolah/all/city/$bbb
+sudo mkdir -p data_sekolah/all/region/$bbb
 echo "startup end"
 
 echo "startup start"
 sudo mkdir -p data_sekolah
-
 wget  -q "https://dapo.kemdikbud.go.id/sp" -O cache/tmp/tiktok.txt 
 xmllint --noout --html -xpath '//*[@id="selectSemester"]/option[1]/@value'  2>/dev/null cache/tmp/tiktok.txt | sed -e "s/ //; s/value=\"//; s/\"//;" > cache/tmp/smstrnow.txt
-bbb=$(cat cache/tmp/smstrnow.txt )
 sudo mkdir -p data_sekolah/all/province/$bbb
 sudo mkdir -p data_sekolah/all/city/$bbb
 sudo mkdir -p data_sekolah/all/region/$bbb
